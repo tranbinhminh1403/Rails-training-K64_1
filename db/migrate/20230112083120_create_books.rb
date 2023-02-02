f9d8a7f619ac0d02bab0fbfc81ad8bbdbe228dad
tranbinhminh1403 <diobrando1403@gmail.com>
2023-02-02 16:35:07 +0700

    add detail

--- db/migrate/20230112083120_create_books.rb
+++ db/migrate/20230112083120_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
     create_table :books do |t|
       t.string :title
       t.integer :number_of_pages
-
+      
       t.timestamps
     end
   end
